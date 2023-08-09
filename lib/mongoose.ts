5592bd6544c1fe4122bd3df9c511e7d36e6b520f
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-08-09 16:33:07 +0200

    mongoDB is setup and connected to mongoose

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -7,5 +7,10 @@ export const connectToDB = async () => {
   if (!process.env.MONGODB_URL) return console.log("MONGO_DB not found");
   if (isConnected) return console.log("Already connected to MongoDB");
   try {
-  } catch (error) {}
+    await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
 };
