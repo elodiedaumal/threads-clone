a44c18abe3e2dd0286a97fcf2fe0930ae9ef9c30
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-08-10 15:39:55 +0200

    update the user info from the database in file action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,27 @@
 "use server";
 
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-export async function updateUser(): Promise<void> {
+export async function updateUser(
+  userId: String,
+  username: string,
+  bio: string,
+  image: string,
+  name: string,
+  path: string
+): Promise<void> {
   connectToDB();
+
+  await User.findOneAndUpdate(
+    { id: userId },
+    {
+      username: username.toLowerCase(),
+      name,
+      bio,
+      image,
+      onboarded: true,
+    },
+    { upsert: true }
+  );
 }
