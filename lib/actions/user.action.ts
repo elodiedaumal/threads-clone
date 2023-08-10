49f024540a6e3b685d2c7f816c9a7cf28cced2bd
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-08-10 15:47:09 +0200

    done with the backend side of the user

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
@@ -12,16 +13,22 @@ export async function updateUser(
   path: string
 ): Promise<void> {
   connectToDB();
-
-  await User.findOneAndUpdate(
-    { id: userId },
-    {
-      username: username.toLowerCase(),
-      name,
-      bio,
-      image,
-      onboarded: true,
-    },
-    { upsert: true }
-  );
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
+    );
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user:${error.message}`);
+  }
 }
