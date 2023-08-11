9358446acedc710f24703c2a8c4155df6af192bc
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-08-11 14:39:59 +0200

    created the pagination in thread.action

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -39,7 +39,27 @@ export async function createThread({
 export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   connectToDB();
 
-  const postsQuery = Thread.find({ parenrId: { $in: [null, undefined] } }).sort(
-    { createdAt: "desc" }
-  );
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const postsQuery = Thread.find({ parenrId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+  const posts = await postsQuery.exec();
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, next };
 }
