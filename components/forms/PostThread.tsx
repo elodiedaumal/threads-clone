268db0638cfc9a972804837901480a9d618ba589
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-08-11 10:19:06 +0200

    start the postthread page with the form

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
 
-const PostThread = ({ userId }) => {
-  return <div>PostThread</div>;
-};
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+
+//  import { updateUser } from "@/lib/actions/user.action";
+// import { UserValidation } from "@/lib/validations/user";
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+function PostThread({ userId }: { userId: string }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+  return <h1>Post Thread form</h1>;
+}
 
 export default PostThread;
