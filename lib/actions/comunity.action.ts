f76d95210420dfc2527924c472c5651884da0af7
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-08-23 15:10:05 +0200

    fixed error in thread tab from community

--- lib/actions/comunity.action.ts
+++ lib/actions/comunity.action.ts
@@ -73,29 +73,30 @@ export async function fetchCommunityDetails(id: string) {
 export async function fetchCommunityPosts(id: string) {
   try {
     connectToDB();
-
-    const communityPosts = await Community.findById(id).populate({
-      path: "threads",
-      model: Thread,
-      populate: [
-        {
-          path: "author",
-          model: User,
-          select: "name image id", // Select the "name" and "_id" fields from the "User" model
-        },
-        {
-          path: "children",
-          model: Thread,
-          populate: {
+    if (id.match(/^[0-9a-fA-F]{24}$/)) {
+      const communityPosts = await Community.findById(id).populate({
+        path: "threads",
+        model: Thread,
+        populate: [
+          {
             path: "author",
             model: User,
-            select: "image _id", // Select the "name" and "_id" fields from the "User" model
+            select: "name image id", // Select the "name" and "_id" fields from the "User" model
           },
-        },
-      ],
-    });
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "image _id", // Select the "name" and "_id" fields from the "User" model
+            },
+          },
+        ],
+      });
 
-    return communityPosts;
+      return communityPosts;
+    }
   } catch (error) {
     // Handle any errors
     console.error("Error fetching community posts:", error);
